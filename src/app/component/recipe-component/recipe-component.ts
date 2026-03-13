dc5661608d5e254083348657039c346b082ea6fd
GaetanoNicotra <nicotragaetano10@gmail.com>
2026-03-13 12:45:52 +0100

    inserimento link delle immagini

--- src/app/component/recipe-component/recipe-component.ts
+++ src/app/component/recipe-component/recipe-component.ts
@@ -14,105 +14,105 @@ export class RecipeComponent {
       ingredienti: ["Spaghetti", "Uova", "Guanciale", "Pecorino Romano", "Pepe"],
       tempoPreparazione: 20,
       difficolta: "Media",
-      immagine: "https://cookidoo.it/recipes/recipe/it/r732007"
+      immagine: "https://www.mamablip.com/storage/Traditional-Spaghetti-alla-Carbonara_1131611747132.jpg"
     },
     {
       nome: "Lasagne alla Bolognese",
       ingredienti: ["Pasta per lasagne", "Ragù", "Besciamella", "Parmigiano"],
       tempoPreparazione: 60,
       difficolta: "Media",
-      immagine: "https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_1280.jpg"
+      immagine: "https://www.sicucina.it/wp-content/uploads/2025/09/Lasagne-alla-Bolognese-Ricetta-Classica-Emblema-della-Tradizione-Italiana.jpg"
     },
     {
       nome: "Pizza Margherita",
       ingredienti: ["Farina", "Pomodoro", "Mozzarella", "Basilico", "Olio"],
       tempoPreparazione: 90,
       difficolta: "Media",
-      immagine: "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg"
+      immagine: "https://assets-eu-01.kc-usercontent.com/6190ffc3-df26-019e-e092-120e0715df95/9c076f13-af22-410c-876d-7986dd243846/Pizza%20margherita.png?w=626&fm=webp&lossless=0&q=80"
     },
     {
       nome: "Risotto ai Funghi",
       ingredienti: ["Riso", "Funghi", "Brodo", "Burro", "Parmigiano"],
       tempoPreparazione: 35,
       difficolta: "Media",
-      immagine: "https://cdn.pixabay.com/photo/2018/04/22/16/27/rice-3347680_1280.jpg"
+      immagine: "https://www.ghiottogalfre.it/50-zoom_default/risotto-ai-funghi-porcini.jpg"
     },
     {
       nome: "Insalata Caprese",
       ingredienti: ["Pomodori", "Mozzarella", "Basilico", "Olio", "Sale"],
       tempoPreparazione: 10,
       difficolta: "Facile",
-      immagine: "https://cdn.pixabay.com/photo/2016/11/02/14/44/italy-1794363_1280.jpg"
+      immagine: "https://www.tavolartegusto.it/wp/wp-content/uploads/2017/07/Insalata-Caprese-Ricetta-originale-Insalata-Caprese-pomodori-e-mozzarella-.jpg"
     },
     {
       nome: "Pollo al Curry",
       ingredienti: ["Petto di pollo", "Curry", "Latte di cocco", "Cipolla", "Riso"],
       tempoPreparazione: 40,
       difficolta: "Media",
-      immagine: "https://cdn.pixabay.com/photo/2018/01/16/19/17/chicken-curry-3087718_1280.jpg"
+      immagine: "https://www.cucchiaio.it/content/cucchiaio/it/ricette/2015/11/pollo-al-curry/jcr:content/imagePreview.img10.jpg/1620744313759.jpg"
     },
     {
       nome: "Hamburger Classico",
       ingredienti: ["Pane", "Carne macinata", "Lattuga", "Pomodoro", "Formaggio"],
       tempoPreparazione: 25,
       difficolta: "Facile",
-      immagine: "https://cdn.pixabay.com/photo/2014/10/23/18/05/burger-500054_1280.jpg"
+      immagine: "https://images-tastehub.mdlzapps.cloud/images/017bedd0-06c9-4461-9eb1-8eb59b50e493.jpg"
     },
     {
       nome: "Tiramisù",
       ingredienti: ["Mascarpone", "Savoiardi", "Caffè", "Uova", "Cacao"],
       tempoPreparazione: 30,
       difficolta: "Media",
-      immagine: "https://cdn.pixabay.com/photo/2017/01/20/15/06/tiramisu-1995536_1280.jpg"
+      immagine: "https://www.bunsenburnerbakery.com/wp-content/uploads/2016/06/easy-tiramisu-square-29-720x405.jpg"
     },
     {
       nome: "Pancake",
       ingredienti: ["Farina", "Uova", "Latte", "Zucchero", "Burro"],
       tempoPreparazione: 15,
       difficolta: "Facile",
-      immagine: "https://cdn.pixabay.com/photo/2016/03/05/19/02/pancakes-1238246_1280.jpg"
+      immagine: "https://www.giallozafferano.it/images/243-24387/Pancakes-allo-sciroppo-d-acero_650x433_wm.jpg"
     },
     {
       nome: "Sushi Rolls",
       ingredienti: ["Riso sushi", "Alga nori", "Salmone", "Avocado"],
       tempoPreparazione: 50,
       difficolta: "Difficile",
-      immagine: "https://cdn.pixabay.com/photo/2017/05/07/08/56/sushi-2291632_1280.jpg"
+      immagine: "https://images.immediate.co.uk/production/volatile/sites/30/2024/01/California-roll-a87199c.jpg"
     },
     {
       nome: "Paella",
       ingredienti: ["Riso", "Frutti di mare", "Pollo", "Zafferano", "Piselli"],
       tempoPreparazione: 60,
       difficolta: "Difficile",
-      immagine: "https://cdn.pixabay.com/photo/2017/05/07/23/06/food-2291898_1280.jpg"
+      immagine: "https://www.cucchiaio.it/content/dam/cucchiaio/it/ricette/2013/07/ricetta-paella-pesce/Paella%20di%20pesce_orizz.jpg"
     },
     {
       nome: "Zuppa di Lenticchie",
       ingredienti: ["Lenticchie", "Carote", "Sedano", "Cipolla", "Brodo"],
       tempoPreparazione: 40,
       difficolta: "Facile",
-      immagine: "https://cdn.pixabay.com/photo/2018/01/04/22/25/lentil-soup-3065770_1280.jpg"
+      immagine: "https://www.soniaperonaci.it/wp-content/uploads/2025/04/Zuppa-di-lenticchie-2.jpg"
     },
     {
       nome: "Frittata di Verdure",
       ingredienti: ["Uova", "Zucchine", "Peperoni", "Cipolla", "Parmigiano"],
       tempoPreparazione: 20,
       difficolta: "Facile",
-      immagine: "https://cdn.pixabay.com/photo/2017/05/07/08/56/omelette-2291450_1280.jpg"
+      immagine: "https://i0.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2015/11/1312_frittata.jpg?fit=1216%2C616&ssl=1"
     },
     {
       nome: "Gnocchi al Pesto",
       ingredienti: ["Gnocchi", "Basilico", "Pinoli", "Parmigiano", "Olio"],
       tempoPreparazione: 25,
       difficolta: "Media",
-      immagine: "https://cdn.pixabay.com/photo/2019/10/15/12/55/gnocchi-4552466_1280.jpg"
+      immagine: "https://qbcucina.com/cdn/shop/articles/img-1713808789633_a37501db-2cb5-404a-9bff-eed4f8c45977_1200x.png?v=1713810591"
     },
     {
       nome: "Cheesecake ai Frutti di Bosco",
       ingredienti: ["Biscotti", "Burro", "Formaggio cremoso", "Zucchero", "Frutti di bosco"],
       tempoPreparazione: 45,
       difficolta: "Media",
-      immagine: "https://cdn.pixabay.com/photo/2018/06/18/16/05/cheesecake-3482749_1280.jpg"
+      immagine: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkZ-eRmOj0iEwJhsfYfOEOgO3FxbQ0MHti-w&s"
     }
   ]
 }

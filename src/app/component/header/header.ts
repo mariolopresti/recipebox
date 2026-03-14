aac7e5dfbbee1df8ff5033ac204dfb2bfeb10046
mariolopresti <mario84ct@hotmail.it>
2026-03-14 17:25:25 +0100

    add directives for filtering recipes by name

--- src/app/component/header/header.ts
+++ src/app/component/header/header.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RecipeService } from '../../services/recipe-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  recipeService = inject(RecipeService);
+}
--- src/app/component/recipe-component/recipe-component.ts
+++ src/app/component/recipe-component/recipe-component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe';
 import { RouterLink } from '@angular/router';
 import { RecipeService } from '../../services/recipe-service';
+import { FilterByName } from '../../directives/filter-by-name';
 
 @Component({
   selector: 'app-recipe-component',
-  imports: [RouterLink],
+  imports: [RouterLink, FilterByName],
   templateUrl: './recipe-component.html',
   styleUrl: './recipe-component.css',
 })

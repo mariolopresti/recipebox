20ed4f8e295de5d376efd978b30bc4eecf316e34
mariolopresti <mario84ct@hotmail.it>
2026-03-16 10:09:23 +0100

    add recipe form component

---
+++ src/app/component/recipe-form-component/recipe-form-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-form-component',
+  imports: [],
+  templateUrl: './recipe-form-component.html',
+  styleUrl: './recipe-form-component.css',
+})
+export class RecipeFormComponent {}
